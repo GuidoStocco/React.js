5d0192fdfce00eed04eaec3d76e1373774e7e95b
unknown <guidostoccodev@gmail.com>
2024-07-28 19:15:12 -0300

    criando componentes

---
+++ 2_fundamentos/fundamentos/src/App.jsx
@@ -0,0 +1,18 @@
+//Components
+import FirstComponent from "./components/FirstComponent";
+import Templete from "./components/TempleteComponents";
+
+//styles //css
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos react</h1>
+      <FirstComponent/>
+      <Templete/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 2_fundamentos/fundamentos/src/components/TempleteComponents.jsx
@@ -0,0 +1,11 @@
+const Templete = () => {
+    const nome = "Guido"    
+    
+    return(
+        <div>
+            <h1>Olá {nome}, tudo bem?</h1>
+        </div>
+    );
+}
+
+export default Templete;
