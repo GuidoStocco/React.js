0792ba54ada2c564e07dce897c82118d40842ab5
unknown <guidostoccodev@gmail.com>
2024-07-29 20:17:21 -0300

    map

--- 3_Avancando/avancado/src/components/ListRender.jsx
+++ 3_Avancando/avancado/src/components/ListRender.jsx
@@ -4,12 +4,26 @@ const ListRender = () => {
 
     const [list] = useState(["Guido", "Matheus", "Lucas"]);
 
+    const [users] = useState([
+        {id:1, nome: 'guido', age: 24},
+        {id:13213, nome: 'gabriel', age: 15},
+        {id:13323, nome: 'diogo', age: 20},
+    ])
+
   return (
+    <div>
     <ul>
         {list.map((item, i) => (
             <li key={i}>{item}</li>
         ))}
     </ul>
+
+    <ul>
+        {users.map((user) => (
+            <li key={user.id}>{user.nome} - {user.age}</li>
+        ))}
+    </ul>
+    </div>
   );
 }
 
