8cac21ce1535b62c93e65d5286baad1e812e276b
unknown <guidostoccodev@gmail.com>
2024-07-29 21:39:00 -0300

    previous state

--- 3_Avancando/avancado/src/components/ListRender.jsx
+++ 3_Avancando/avancado/src/components/ListRender.jsx
@@ -4,11 +4,19 @@ const ListRender = () => {
 
     const [list] = useState(["Guido", "Matheus", "Lucas"]);
 
-    const [users] = useState([
+    const [users, setUsers] = useState([
         {id:1, nome: 'guido', age: 24},
-        {id:13213, nome: 'gabriel', age: 15},
-        {id:13323, nome: 'diogo', age: 20},
+        {id:2, nome: 'gabriel', age: 15},
+        {id:3, nome: 'diogo', age: 20},
     ])
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevUsers) =>{
+            return prevUsers.filter((user) => randomNumber !== user.id)
+        })
+    }
 
   return (
     <div>
@@ -23,6 +31,7 @@ const ListRender = () => {
             <li key={user.id}>{user.nome} - {user.age}</li>
         ))}
     </ul>
+    <button onClick={deleteRandom}>Delete random user</button>
     </div>
   );
 }
