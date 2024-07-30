c9616feaac22b901a5efe87fb351d655279da4e5
unknown <guidostoccodev@gmail.com>
2024-07-30 19:14:16 -0300

    conditional

--- 3_Avancando/avancado/src/components/ConditionalRender.jsx
+++ 3_Avancando/avancado/src/components/ConditionalRender.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useState } from "react"
 
 const ConditionalRender = () => {
+
+    const [x] = useState(true);
+
+    const [name] = useState("João");
+
   return (
-    <div>ConditionalRender</div>
+    <div>
+        <h1>Isso será exibido?</h1>
+        {x && <p>Se x for true, sim!</p>}
+
+        {/* if e else no jsx/ condição ? bloco : bloco 2 */}
+        {name === "João" ? (
+            <div>
+                <p>O nome é joão</p>
+            </div>    
+        ) : (
+            <div>
+                <p>Nome não encontrado</p>
+            </div>    
+        )}
+
+    </div>
   )
 }
 
