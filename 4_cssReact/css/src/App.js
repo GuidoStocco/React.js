96b50be21f48776f6039cbcb37c4fd81430e91f4
unknown <guidostoccodev@gmail.com>
2024-07-31 20:39:30 -0300

    classe dinâmica css

--- 4_cssReact/css/src/App.js
+++ 4_cssReact/css/src/App.js
@@ -2,10 +2,14 @@ import MyComponents from "./components/MyComponents";
 
 
 function App() {
+  const redTitle = true;
   return (
     <div className="App">
       <h1>CSS REACT</h1>
+      {/* css de componente */}
       <MyComponents/>
+      {/* Classe dinâmica no css */}
+      <h2 className={redTitle ? "red-title" : "title"}>classe dinâmica no css</h2>
     </div>
   );
 }
