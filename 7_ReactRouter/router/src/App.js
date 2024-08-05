edbd1a29d37d3f3d22d06a5eeee777bb6d262c1e
unknown <guidostoccodev@gmail.com>
2024-08-05 20:58:08 -0300

    React Router dom

--- 7_ReactRouter/router/src/App.js
+++ 7_ReactRouter/router/src/App.js
@@ -1,10 +1,23 @@
-
+// css
 import './App.css';
+
+// config react router
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+//pages
+import Home from './pages/Home';
+import About from './pages/About';
+
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
