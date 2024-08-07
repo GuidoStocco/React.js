275baf66e3934fb346d079f6df3d2c9555eec883
unknown <guidostoccodev@gmail.com>
2024-08-06 21:57:22 -0300

    rotas dinâmicas

--- 7_ReactRouter/router/src/App.js
+++ 7_ReactRouter/router/src/App.js
@@ -7,6 +7,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 //pages
 import Home from './pages/Home';
 import About from './pages/About';
+import Product from './pages/Product';
 
 //components
 import Navbar from './components/Navbar';
@@ -21,6 +22,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
+          <Route path='/products/:id' element={<Product/>}/>
         </Routes>
       </BrowserRouter>
     </div>
