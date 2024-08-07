608f4f20257c75672ab55911e7d26672a7a79c2b
unknown <guidostoccodev@gmail.com>
2024-08-07 17:54:58 -0300

    context api

---
+++ 8_contextAPI/context/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+      <nav>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/about'>Sobre</NavLink>
+          <NavLink to='/products'>Produtos</NavLink>
+      </nav>  
+    )
+  }
+  
+  export default Navbar
